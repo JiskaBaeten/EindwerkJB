b6c58566e824a5c4c8aa381efe4316e2e78ab2a7
Jiska Baeten <jiska.baeten@student.kdg.be>
2017-02-26 17:39:46 +0100

    LoadScreenScript that only works on PC
    add button to scene if you want to work with buttons (and put script
    onto the button)

---
+++ Katamari Game/Assets/01 scripts/LoadTargetScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadTargetScene : MonoBehaviour { //werkt wel op PC
+  public int numStartUpScene = 1;
+  public int secondsToWait = 1;
+
+  private void Start()
+  {
+     StartCoroutine("WaitAMoment");
+  }
+
+  IEnumerator WaitAMoment()
+  {
+    yield return new WaitForSeconds(secondsToWait); //wait till Loading is ready too
+    LoadTarget();
+  }
+
+  public void LoadTarget()
+  {
+    LoadingScreenManager.LoadScene(numStartUpScene);
+  }
+}
